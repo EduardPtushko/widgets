cd05dccdcd56c58173f9ad7c52063e7b03a09e21
Eduard Ptushko <ptushko@gmail.com>
2020-09-08 12:27:03 +0300

    add App component

---
+++ src/App/index.tsx
@@ -0,0 +1,50 @@
+import React, { Suspense, lazy } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { hot } from 'react-hot-loader/root';
+const Elements = lazy(() => import('../Elements'));
+const Collections = lazy(() => import('../Collections'));
+const NotFound = lazy(() => import('../shared/NotFound'));
+const Views = lazy(() => import('../Views'));
+const Modules = lazy(() => import('../Modules'));
+import Menu from '../Menu';
+import Home from '../shared/Home';
+import ErrorBoundary from '../shared/Error/ErrorBoundary';
+import Spinner from '../shared/Spinner/Spinner';
+
+const App: React.FC = () => {
+    return (
+        <>
+            <div className='ui container'>
+                <Menu />
+                <div className='segment'>
+                    <ErrorBoundary>
+                        <Suspense fallback={<Spinner />}>
+                            <Switch>
+                                <Route exact path='/'>
+                                    <Home />
+                                </Route>
+                                <Route path='/elements'>
+                                    <Elements />
+                                </Route>
+                                <Route path='/collections'>
+                                    <Collections />
+                                </Route>
+                                <Route path='/views'>
+                                    <Views />
+                                </Route>
+                                <Route path='/modules'>
+                                    <Modules />
+                                </Route>
+                                <Route path='*'>
+                                    <NotFound />
+                                </Route>
+                            </Switch>
+                        </Suspense>
+                    </ErrorBoundary>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default hot(App);
---
+++ src/App/test.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+import App from './';
+
+// eslint-disable-next-line react/display-name
+jest.mock('../shared/Home', () => () => <div>Home</div>);
+
+describe('App', () => {
+    test('renders successfully', () => {
+        const history = createMemoryHistory();
+        const { container } = render(
+            <Router history={history}>
+                <App />
+            </Router>,
+        );
+
+        expect(container.innerHTML).toMatch('Elements');
+        expect(container.innerHTML).toMatch('Collections');
+        expect(container.innerHTML).toMatch('Views');
+        expect(container.innerHTML).toMatch('Modules');
+    });
+
+    test('renders Home component on root route', () => {
+        const { container } = renderWithRouter(() => <App />, '/');
+
+        expect(container.innerHTML).toMatch('Home');
+    });
+
+    test('renders Elements component on /elements route', async () => {
+        renderWithRouter(() => <App />, '/elements');
+
+        const component = await screen.findByText(/placeholder component/i);
+
+        expect(component).toBeInTheDocument();
+    });
+
+    test('renders Collections component on /collections route', async () => {
+        renderWithRouter(() => <App />, '/collections');
+
+        const component = await screen.findByText(/table component/i);
+
+        expect(component).toBeInTheDocument();
+    });
+
+    test('renders Views component on /views route', async () => {
+        renderWithRouter(() => <App />, '/views');
+
+        const component = await screen.findByText(/statistics component/i);
+
+        expect(component).toBeInTheDocument();
+    });
+
+    test('renders Modules component on /modules route', async () => {
+        renderWithRouter(() => <App />, '/modules');
+
+        const component = await screen.findByText(/modal component/i);
+
+        expect(component).toBeInTheDocument();
+    });
+
+    test('renders NotFound component on /unknown route', async () => {
+        renderWithRouter(() => <App />, '/unkown');
+
+        const component = await screen.findByText(/Oops...Page not found/i);
+
+        expect(component).toBeInTheDocument();
+    });
+});
